2b7238284b98ebc52d1697595ac11e967f5c0837
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2021-03-07 03:40:45 +0900

    ping pong

--- main.go
+++ main.go
@@ -13,17 +13,6 @@ var (
 	token = "Bot " + os.Getenv("BOT_TOKEN")
 )
 
-var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-	"hello": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionApplicationCommandResponseData{
-				Content: "Hello!",
-			},
-		})
-	},
-}
-
 func main() {
 	log.Print("start")
 	defer log.Print("end")
@@ -42,28 +31,22 @@ func execute() error {
 	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Println("bot is ready")
 	})
-	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commandHandlers[i.Data.Name]; ok {
-			h(s, i)
+
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
 		}
+
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "Pong!")
+		}
+
 	})
 
 	if err := discord.Open(); err != nil {
 		return fmt.Errorf("opening websocket connection")
 	}
 	defer discord.Close()
-
-	log.Print("register hello command")
-	if _, err := discord.ApplicationCommandCreate(
-		discord.State.User.ID,
-		"",
-		&discordgo.ApplicationCommand{
-			Name:        "hello",
-			Description: "Say Hello",
-		},
-	); err != nil {
-		return fmt.Errorf("creating application command: %w", err)
-	}
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
