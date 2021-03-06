a776281c37dc322fccffa8271ee5c37ae1e08d47
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2021-03-07 01:07:09 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package p
+
+import (
+	"crypto/ed25519"
+	"net/http"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var applicationPublicKey ed25519.PublicKey
+
+func init() {
+	appPubKeyEnv := os.Getenv("APPLICATION_PUBLICKEY")
+	if appPubKeyEnv == "" {
+		panic("environment variable APPLICATION_PUBLICKEY is not set")
+	}
+
+	applicationPublicKey = ed25519.PublicKey([]byte(appPubKeyEnv))
+}
+
+// Handler is a http handler for Cloud Functions.
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if !discordgo.VerifyInteraction(r, applicationPublicKey) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("invalid request signature"))
+		return
+	}
+}
